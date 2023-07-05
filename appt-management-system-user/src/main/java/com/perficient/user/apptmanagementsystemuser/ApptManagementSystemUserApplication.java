b30166507637b0d4f19b9dc0cb9224b9708d5c64
sunishaarora <sunisha.arora1309@gmail.com>
2023-07-05 15:46:17 -0500

    Initial commit

---
+++ appt-management-system-user/src/main/java/com/perficient/user/apptmanagementsystemuser/ApptManagementSystemUserApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.user.apptmanagementsystemuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptManagementSystemUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptManagementSystemUserApplication.class, args);
+	}
+
+}
---
+++ appt-management-system-user/src/test/java/com/perficient/user/apptmanagementsystemuser/ApptManagementSystemUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.user.apptmanagementsystemuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptManagementSystemUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
