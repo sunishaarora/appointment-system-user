ca23bdad06d174b39f28c3cb8ac33f952dd71ffe
aashi-agr <aashi.agrawal@perficient.com>
2023-07-11 11:44:06 -0500

    Test cases controller layer for ListUsers

---
+++ appt-management-system-user/src/test/java/com/perficient/user/apptmanagementsystemuser/controller/UserListUsersControllerTest.java
@@ -0,0 +1,52 @@
+package com.perficient.user.apptmanagementsystemuser.controller;
+
+import com.perficient.user.apptmanagementsystemuser.model.User;
+import com.perficient.user.apptmanagementsystemuser.service.UserListUsersService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class UserListUsersControllerTest {
+    private MockMvc mockMvc;
+    @Mock
+    private UserListUsersService userListUsersService;
+    @InjectMocks
+    private UserListUsersController userListUsersController;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(userListUsersController).build();
+    }
+
+    @Test
+    void ListUsers_Success(){
+        List<User> userList = new ArrayList<>();
+        when(userListUsersService.getAllUsers()).thenReturn(userList);
+        ResponseEntity<List<User>> response = userListUsersController.getAllUsers();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(userList, response.getBody());
+    }
+
+    @Test
+    void ListUsers_EmptyListException(){
+        String errorMessage = "No users found";
+        EmptyListException exception = new EmptyListException(errorMessage);
+        ResponseEntity<String> response = userListUsersController.handleEmptyListException(exception);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        assertEquals(errorMessage, response.getBody());
+    }
+}
